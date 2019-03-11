41b55ab5e9b285b1d4a13efa93155bbe9d6a3ee8
Luigi Mori <l@isidora.org>
2019-03-11 22:59:28 +0100

    Improves deploy
    Signed-off-by: Luigi Mori <l@isidora.org>

--- scripts/deploy-image.sh
+++ scripts/deploy-image.sh
@@ -3,7 +3,7 @@ echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 echo "Pushing jtschichold/minemeld:$TRAVIS_BRANCH"
 docker push jtschichold/minemeld:"$TRAVIS_BRANCH"
 
-if [[ -z "${TRAVIS_TAG}" ]]; then
+if [[ -n "${TRAVIS_TAG}" ]]; then
     docker tag jtschichold/minemeld:"$TRAVIS_BRANCH" jtschichold/minemeld:"$TRAVIS_TAG"
     echo "Pushing jtschichold/minemeld:$TRAVIS_TAG"
     docker push jtschichold/minemeld:"$TRAVIS_TAG"
